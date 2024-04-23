c588e8752feef9128fedda45cd60d9ba271021aa
Ryan Jackson <rmjackson234@gmail.com>
2024-04-23 17:36:51 -0600

    final

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,16 @@ import { createStore } from "vuex"
 import { auth } from "./auth.module"
 import { user } from "./user.module"
 import { movies } from "./movie.module"
+import { directors } from "./director.module"
+import { actors } from "./actor.module"
 
 const store = createStore({
 	modules: {
 		auth,
 		user,
-		movies
+		movies,
+		directors,
+		actors
 	}
 })
 
